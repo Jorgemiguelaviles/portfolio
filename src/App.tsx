cd1bf26a75b8f4a98945176c044acb399196736c
jorge <jogadordaverdade@gmail.com>
2024-09-07 13:03:20 -0300

    my first commit to portfolio construction

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+
+import React from 'react';
+import HomePage from './pages/main/index';
+
+function App() {
+  return (
+    <>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,14 @@
+// src/pages/index.tsx
+
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is the content of the page.</p>
+    </div>
+  );
+}
+
+export default HomePage;
